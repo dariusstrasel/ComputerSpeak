004b5b05383e2fca56eb926ce1602da84fcb5a81
Dstrasel <siruku@gmail.com>
2017-03-27 15:48:09 -0400

    Starter TODO for documenting architecture.

---
+++ todo.py
@@ -0,0 +1,18 @@
+########################################################################################################################
+# TODO Setup gulp.js environment
+# TODO Finish CSS draft for HTML
+# -- Black theme with green text ala Matrix; monospace font
+# -- Blue theme with white/black text; minimal Google Material style; Roboto-sans
+# TODO Write JavaScript client-form validation code
+# -- Add DOMEvents mutation watchers for source code modification; reload if True
+# TODO Write JavaScript animations?
+# -- Make the header h1 switch from ASCII to random values.
+# TODO Write Flask-wertzueg server backend
+# -- Receive HTTP form inputs and route thru Python converter functions
+# -- Implement controller logic for handling polymorphic type conversions
+# -- Implement logic for validating textual input data?
+# TODO Deploy unto Heroku
+# ----------------------------------------------------------------------------------------------------------------------
+# TODO OPTIONAL: Add encryption option for encrypting output conversions for funsies.
+# -- How to handle back/forth encryption?
+########################################################################################################################
