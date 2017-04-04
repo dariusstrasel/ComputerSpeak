3e85ff1a17c1db274e26aba07894c15f3baa0591
Darius Strasel <strasel.darius@gmail.com>
2017-04-04 19:00:05 -0400

    Python virtualenv

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dariusstrasel/GoogleDrive/Programming/Git/ComputerSpeak/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
