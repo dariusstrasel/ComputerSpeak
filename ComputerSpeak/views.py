f6448daad949bb265b96d19915429e714b9f413f
Darius Strasel <strasel.darius@gmail.com>
2017-04-04 20:24:23 -0400

    Imported input_converter functions and added http handling to translate view.

--- ComputerSpeak/views.py
+++ ComputerSpeak/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .input_converters import *
 
 
 def home(request):
@@ -7,4 +8,11 @@ def home(request):
 
 
 def translate(request):
-    return HttpResponse("You're on the translate page.")
+    input_text = request.GET['inputText']
+    input_type = request.GET['input_types']
+
+    output_type = request.GET['output_types']
+    output_text = convert_inputs(input_text, input_type, output_type)
+
+    return render(request, 'index.html', {'output_text': output_text, 'input_text': input_text})
+    # return HttpResponse("You're on the translate page. " + translation)
