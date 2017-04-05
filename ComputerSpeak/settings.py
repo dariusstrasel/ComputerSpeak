56a66250fc1d68baabae81d20730cbb0ebd5146e
Darius Strasel <strasel.darius@gmail.com>
2017-04-05 17:39:39 -0400

    Adjusted static template directory.

--- ComputerSpeak/settings.py
+++ ComputerSpeak/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -116,5 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_URL = '/templates/'
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR),
+    'templates/build/css/',
+    'templates/build/js/',
+]
+print(STATICFILES_DIRS)
