1aae0b9fa2900d86812ddfe71f89e34636e523dd
Darius Strasel <strasel.darius@gmail.com>
2017-04-05 17:40:09 -0400

    Adjusted translate handler to return JSON.

--- ComputerSpeak/views.py
+++ ComputerSpeak/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 from .input_converters import *
 
@@ -8,11 +9,16 @@ def home(request):
 
 
 def translate(request):
+    print(request)
     input_text = request.GET['inputText']
+    print(input_text)
     input_type = request.GET['input_types']
 
     output_type = request.GET['output_types']
     output_text = convert_inputs(input_text, input_type, output_type)
 
-    return render(request, 'index.html', {'output_text': output_text, 'input_text': input_text})
-    # return HttpResponse("You're on the translate page. " + translation)
+    data = {'output_text': output_text, 'input_text': input_text}
+    print(data)
+    #return render(request, 'index.html', data)
+    #return HttpResponse(data)
+    return JsonResponse(data)
